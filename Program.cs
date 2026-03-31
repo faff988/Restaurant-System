c262bcf4dfeea9e4f59ea04bc01a66a921c22cad
vonmanteks <hekh96880@gmail.com>
2026-03-31 07:47:24 +0700

    Fix auto migrate

--- Program.cs
+++ Program.cs
@@ -44,6 +44,9 @@ app.MapControllerRoute(
 
 using (var scope = app.Services.CreateScope())
 {
+    var db = scope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
+    db.Database.Migrate();
+
     var roleManager = scope.ServiceProvider.GetRequiredService<RoleManager<IdentityRole>>();
     var userManager = scope.ServiceProvider.GetRequiredService<UserManager<IdentityUser>>();
 
